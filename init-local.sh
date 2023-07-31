1ca6753173c3c467c94fa642c16b6ee852c2a919
iprotoni <0xdevnet.eth@gmail.com>
2023-07-31 02:46:14 -0400

    Add init-local.sh

---
+++ init-local.sh
@@ -0,0 +1,13 @@
+NAMESPACE_ID=$(openssl rand -hex 8)
+echo $NAMESPACE_ID
+
+DA_BLOCK_HEIGHT=$(curl https://rpc-arabica-9.consensus.celestia-arabica.com/block |jq -r '.result.block.header.height')
+echo $DA_BLOCK_HEIGHT
+
+AUTH_TOKEN=""
+echo $AUTH_TOKEN
+
+ethermintd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://localhost:26658","timeout":60000000000,"fee":600000,"gas_limit":6000000,"auth_token":"'$AUTH_TOKEN'"}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT
+
+# uncomment the next command if you are using lazy aggregation
+# ethermintd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://localhost:26658","timeout":60000000000,"fee":600000,"gas_limit":6000000,"auth_token":"'$AUTH_TOKEN'"}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT --rollkit.lazy_aggregator
